828175c96ff80fdc45286837bea347cc054f3cc6
DonizeteVida <donizetevida1998@outlook.com>
2023-02-21 21:19:02 -0300

    Add compose bom configuration

--- app/src/main/java/com/app/planmyplant/MainActivity.kt
+++ app/src/main/java/com/app/planmyplant/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.app.planmyplant
 
-import android.Manifest
-import android.app.IntentService
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -17,7 +15,6 @@ import com.app.planmyplant.ui.theme.PlanMyPlantTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Manifest.permission.POST_NOTIFICATIONS
         setContent {
             PlanMyPlantTheme {
                 // A surface container using the 'background' color from the theme
